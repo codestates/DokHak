c35f8cee50d2aa873237d49bbbbd556a05589927
goodbsm2421 <goodbsm2421@gmail.com>
2021-08-26 13:11:20 +0900

    :tada: DB setup

---
+++ server/models/post.js
@@ -0,0 +1,56 @@
+'use strict';
+const { Model } = require('sequelize');
+const posts = require('../controller/posts');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Post.belongsTo(models.User, {
+        foreignKey: 'userId',
+        targetKey: 'id',
+        onDelete: 'cascade',
+      });
+      models.Post.hasMany(models.Comment, {
+        foreignKey: 'postId',
+        sourceKey: 'id',
+        onDelete: 'cascade',
+      });
+      models.Post.belongsToMany(models.Stack, {
+        through: 'post_stack',
+      });
+    }
+  }
+  Post.init(
+    {
+      image: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Post',
+      tableName: 'Posts',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    }
+  );
+  return Post;
+};
