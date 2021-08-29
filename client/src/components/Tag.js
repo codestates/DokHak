e5874acd1dc178e0ba160d8b95de0694abc8e37e
seona-jung <seona.jung.job@gmail.com>
2021-08-29 16:30:12 +0900

    :white_check_mark: Add: PostDetail Page

---
+++ client/src/components/Tag.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTag = styled.label`
+  padding: 5px 20px;
+  border-radius: 10px;
+  letter-spacing: 0.05em;
+  text-decoration: none;
+  font-size: 0.5rem;
+  transition: all 1s;
+  background: #37373e;
+  color: #fff;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+`;
+
+const Tag = ({ children }) => {
+  return (
+    <>
+      <StyledTag>{children}</StyledTag>
+    </>
+  );
+};
+
+export default Tag;
--- client/src/pages/PostDetail.js
+++ client/src/pages/PostDetail.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Tag from '../components/Tag';
 
 import { Main } from './styles';
 
+const Title = styled.h1`
+  width: 100%;
+  border: none;
+  margin-bottom: 1.5rem;
+  outline: none;
+  font-size: 2rem;
+
+  @media (max-width: 768px) {
+    margin-right: 0;
+  }
+`;
+
+const TagAuthorWrapper = styled.div`
+  /* width: inherit; */
+  display: flex;
+  text-align: center;
+  outline: none;
+  margin-top: 5px;
+  margin-bottom: 40px;
+
+  & > :first-child {
+    margin-right: calc(10px + 20px);
+  }
+
+  & > :last-child {
+    margin-left: auto;
+    flex-shrink: 0;
+    line-height: 1;
+  }
+`;
+
+const TagWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  row-gap: 1rem;
+  column-gap: 1rem;
+`;
+
 const PostDetail = () => {
+  const [value, setValue] = useState(`# Hello World
+
+**Hello world!!!**
+
+![](https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN)
+
+\`\`\`javascript
+import React from "react";
+import ReactDOM from "react-dom";
+import MEDitor from '@uiw/react-md-editor';
+
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  return (
+    <div className="container">
+      <MEDitor
+        value={value}
+        onChange={setValue}
+      />
+      <MDEditor.Markdown source={value} />
+    </div>
+  );
+}
+\`\`\``);
+
   return (
     <Main>
       {/* 게시글 본문 */}
+      <Title>별 헤는 밤</Title>
+      <TagAuthorWrapper>
+        <TagWrapper>
+          <Tag>React</Tag>
+          <Tag>Vue.js</Tag>
+          <Tag>Angular</Tag>
+          <Tag>Node.js</Tag>
+          <Tag>Django</Tag>
+          <Tag>Spring</Tag>
+          <Tag>Flutter</Tag>
+          <Tag>React Native</Tag>
+        </TagWrapper>
+        <h3>김코딩</h3>
+      </TagAuthorWrapper>
+      <MDEditor.Markdown source={value} />
       {/* Comment Component */}
     </Main>
   );
