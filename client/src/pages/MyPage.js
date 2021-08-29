0cde5259678f9cefc61e651e7e5a179e2bb59701
chloemk <minsung.chloe.kim@gmail.com>
2021-08-29 21:39:59 +0900

    :white_check_mark: Update: delete comment

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -72,37 +72,6 @@ const SmallTitle = styled.label`
   }
 `;
 
-// const Button = styled.button`
-//   height: 35px;
-//   background-color: #37373e;
-//   border-radius: 5px;
-//   transition: all 0.3s ease 0s, width 0s;
-//   outline: none;
-//   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-//   border: none;
-//   letter-spacing: 2.5px;
-//   font-size: 1rem;
-//   color: #fff;
-//   margin: 5px 0;
-
-//   &:hover {
-//     background-color: #ffc700;
-//     cursor: pointer;
-//     box-shadow: 5px 12px 9px 1px rgba(55, 55, 62, 0.29);
-//     color: #fff;
-//     transform: translateY(-2px);
-
-//     &:active {
-//       transform: scale(1);
-//       box-shadow: none;
-//       outline: 0;
-//     }
-//   }
-//   &:nth-child(5) {
-//     margin-top: 30px;
-//   }
-// `;
-
 const Textarea = styled.textarea`
   margin-bottom: 20px;
   font-size: 16px;
