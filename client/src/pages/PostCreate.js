d8d67eec8d218ffedc6a0c8edb00cfdc96cc6f59
seona-jung <seona.jung.job@gmail.com>
2021-08-30 22:41:00 +0900

    :white_check_mark: Update: Post & User

--- client/src/pages/PostCreate.js
+++ client/src/pages/PostCreate.js
@@ -1,4 +1,6 @@
 import React, { useState, useCallback } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 import styled from 'styled-components';
 import MDEditor from '@uiw/react-md-editor';
 
@@ -7,6 +9,17 @@ import { thumbnails } from '../data';
 import Thumbnail from '../components/Thumbnail';
 import Checkbox from '../components/Checkbox';
 import Button from '../components/Button';
+
+const stacksD = [
+  'React',
+  'Vue.js',
+  'Angular',
+  'Node.js',
+  'Django',
+  'Spring',
+  'Flutter',
+  'React Native',
+];
 
 export const FlexBoxSpaceBetween = styled.div`
   display: flex;
@@ -32,31 +45,57 @@ const TitleInput = styled.input`
   }
 `;
 
-const PostButtonWrapper = ({ className, children }) => (
-  <div className={className}>
-    <Button>{children}</Button>
-  </div>
-);
-
-const PostButton = styled(PostButtonWrapper)`
-  @media (max-width: 768px) {
-    width: 100%;
-    margin: 0;
-    margin-bottom: 30px;
-
-    * {
-      width: 100%;
-      margin: 0;
-    }
-  }
-`;
-
 const PostCreate = () => {
-  const [selectedThumbnail, setSelectedThumbnail] = useState(0);
-  const [value, setValue] = useState('Type here...');
+  const [image, setImage] = useState(0);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [checkedStacks, setCheckedStacks] = useState(
+    Array(stacksD.length).fill(false)
+  );
 
-  const onClickThumbnail = useCallback((idx) => {
-    setSelectedThumbnail(idx);
+  const onClickImage = (idx) => {
+    setImage(idx);
+  };
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeStackCheckbox = (position) => {
+    const updatedCheckedStacks = checkedStacks.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedStacks(updatedCheckedStacks);
+  };
+  const onClickBtn = (data) => {
+    // API 쏘세요!
+    // POST | Add a post (cookie / stacks, image, title, content)
+    const stacks = data.checkedStacks
+      .map((checkedStack, idx) => (checkedStack ? idx + 1 : null))
+      .filter((x) => x);
+    data.stacks = stacks;
+    delete data.checkedStacks;
+    console.log(data);
+  };
+
+  useEffect(async () => {
+    console.log('useEffect');
+    try {
+      const post = await axios.get(
+        `http://ec2-3-34-123-164.ap-northeast-2.compute.amazonaws.com/posts`
+      );
+      console.log(post);
+      // const stacks = await axios.get(`https://dokhak.tk/stacks`);
+      // const stacks = ['React', 'Vue.js', 'Angular', 'Node.js', 'Django'];
+
+      const { title, content, username, stackIds } = post;
+      setTitle(title);
+      setContent(content);
+      setUsername(username);
+      setStacks(stackIds.map((stackId) => stacks[stackId - 1]));
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   return (
@@ -72,20 +111,28 @@ const PostCreate = () => {
             key={src}
             idx={idx}
             src={src}
-            selectedThumbnail={selectedThumbnail}
-            setSelectedThumbnail={onClickThumbnail}
+            image={image}
+            onClickImage={onClickImage}
           />
         ))}
       </FlexBoxSpaceBetween>
 
       <FlexBoxSpaceBetween>
-        <TitleInput placeholder="Title" />
-        <PostButton>확인</PostButton>
+        <TitleInput
+          placeholder="Title"
+          value={title}
+          onChange={(e) => onChangeTitle(e)}
+        />
+
+        <Button
+          onClick={() => onClickBtn({ checkedStacks, image, title, content })}
+        >
+          확인
+        </Button>
       </FlexBoxSpaceBetween>
 
-      <Checkbox />
-
-      <MDEditor value={value} onChange={setValue} height={400} />
+      <Checkbox stacks={stacksD} onChange={onChangeStackCheckbox} />
+      <MDEditor value={content} onChange={setContent} height={400} />
     </Main>
   );
 };
