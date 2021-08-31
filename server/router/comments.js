6b068350755354f58f870a34676c08abfe16fce3
do8972 <do8972@gmail.com>
2021-09-01 02:10:02 +0900

    update: cookie2

--- server/router/comments.js
+++ server/router/comments.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const commentsController = require('../controller/comments');
+const isAuth = require('../middleware/checkToken');
 
 const router = express.Router();
 
-router.post('/:id', commentsController.addComment);
+router.post('/:id', isAuth, commentsController.addComment);
 router.get('/:id', commentsController.getCommentsByPostId);
-router.patch('/:id', commentsController.updateComment);
-router.delete('/:id', commentsController.deleteComment);
+router.patch('/:id', isAuth, commentsController.updateComment);
+router.delete('/:id', isAuth, commentsController.deleteComment);
 
 module.exports = router;
