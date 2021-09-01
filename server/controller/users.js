2994aac447cc44898f7fd5f59874fd122b751c39
do8972 <do8972@gmail.com>
2021-09-01 13:42:08 +0900

    update: users-api

--- server/controller/users.js
+++ server/controller/users.js
@@ -103,7 +103,7 @@ module.exports = {
   logout: async (req, res) => {
     try {
       res.clearCookie('jwt');
-      return res.status(200).json({ message: 'OK' });
+      return res.status(200).json({ message: 'OK' }).redirect('/users'); 
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: 'Server Error' });
